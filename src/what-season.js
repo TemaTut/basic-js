efca139d15b452ba68af1d0bb5155040d715bc68
Tematut <0009494@mail.ru>
2023-04-17 22:11:28 +0400

    --amend

--- src/what-season.js
+++ src/what-season.js
@@ -11,31 +11,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
-
-// function getSeason(date) {
-//   if (!date) {
-//     return 'Unable to determine the time of year!';
-//   }
-//   if (!(date instanceof Date) || isNaN(date)) {
-//     throw new Error("Invalid date!");
-//   }
-//   const myMounth = date.getMonth();
-
-//   switch (myMounth) {
-//     case 1, 0, 11:
-//       return 'winter';
-//     case 2, 3, 4:
-//       return 'spring';
-//     case 7, 5, 6:
-//       return 'summer';
-//     case 10, 9, 8:
-//       return 'autumn';
-//   }
+// function getSeason(/* date */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
 // }
+
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (!(date instanceof Date)) {
+    throw new Error("Invalid date!");
+  }
+  if (isNaN(date) || Object.getOwnPropertyNames(date).includes('toString')) {
+    throw new Error("Invalid date!");
+  }
+
+  const myMounth = date.getMonth();
+
+  switch (myMounth) {
+    case 1: return 'winter';
+    case 0: return 'winter';
+    case 11: return 'winter';
+    case 2: return 'spring';
+    case 3: return 'spring';
+    case 4: return 'spring';
+    case 5: return 'summer';
+    case 6: return 'summer';
+    case 7: return 'summer';
+    case 8: return 'autumn';
+    case 9: return 'autumn';
+    case 10: return 'autumn';
+
+
+  }
+}
 
 module.exports = {
   getSeason
